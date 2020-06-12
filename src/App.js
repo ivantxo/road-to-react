5a0cef8818bc02332366e9da5091322703ad0063
ivantxo <ivan.ramirezf@gmail.com>
2020-06-12 14:39:24 +1000

    Addded props

--- src/App.js
+++ src/App.js
@@ -1,56 +1,62 @@
 import React from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: 'PHP',
-    url: 'https://www.php.net',
-    author: 'Rasmus Lerdorf',
-    num_comments: 3,
-    points: 4,
-    objectID: 2,
-  },
-  {
-    title: 'git',
-    url: 'https://git-scm.com/',
-    author: 'Linus Torvalds',
-    num_comments: 2,
-    points: 6,
-    objectID: 3,
-  },
-];
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+    {
+      title: 'PHP',
+      url: 'https://www.php.net',
+      author: 'Rasmus Lerdorf',
+      num_comments: 3,
+      points: 4,
+      objectID: 2,
+    },
+    {
+      title: 'git',
+      url: 'https://git-scm.com/',
+      author: 'Linus Torvalds',
+      num_comments: 2,
+      points: 6,
+      objectID: 3,
+    },
+  ];
+  
+  const handleChange = event => {
+    console.log(event.target.value);
+  };
 
-const App = () => (
-  <div>
-    <h1>My Hacker Stories</h1>
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
 
-    <hr />
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" />
+      <hr />
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
 
-    <List />
-    <hr />
-  </div>
-)
+      <List list={stories} />
+      <hr />
+    </div>
+  )
+}
 
-const List = () =>
-  list.map((item) => (
+const List = props =>
+  props.list.map((item) => (
       <div key={ item.objectID }>
         <span>
           <a href={item.url}>{item.title}</a>&nbsp;-&nbsp;
