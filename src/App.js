984b1d270db1475b5c1299a2f55655c38bebe5b0
ivantxo <ivan.ramirezf@gmail.com>
2020-06-25 06:30:29 +1000

    Using fragment

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ const App = () => {
 }
 
 const Search = ({ search, onSearch }) => (
-  <div>
+  <>
     <label htmlFor="search">Search: </label>
     <input 
       id="search" 
@@ -85,7 +85,7 @@ const Search = ({ search, onSearch }) => (
       value={search}
       onChange={onSearch}
     />
-  </div>
+  </>
 );
 
 // const List = ({ list }) =>
