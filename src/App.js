4f53242b8374d25bce8b25cfd43ec633d00ffc7a
ivantxo <ivan.ramirezf@gmail.com>
2020-06-30 06:51:07 +1000

    Using useReduce hook

--- src/App.js
+++ src/App.js
@@ -56,10 +56,30 @@ const useSemiPersistentState = (key, initialState) => {
   return [value, setValue];
 };
 
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_STORIES':
+      return action.payload;
+
+    case 'REMOVE_STORY':
+      return state.filter(
+        story => action.payload.objectID !== story.objectID
+      );
+
+    default:
+      throw new Error();
+  }
+};
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', '');
 
-  const [stories, setStories] = React.useState([]);
+  // const [searchTerm, setSearchTerm] = React.useState('');
+
+  const [stories, dispatchStories] = React.useReducer(
+    storiesReducer,
+    []
+  );
 
   const [isLoading, setIsLoading] = React.useState(false);
 
@@ -69,17 +89,20 @@ const App = () => {
     setIsLoading(true);
     getAsyncStories()
       .then(result => {
-        setStories(result.data.stories);
+        dispatchStories({
+          type: 'SET_STORIES',
+          payload: result.data.stories,
+        });
         setIsLoading(false);
       })
       .catch(() => setIsError(true));
   }, []);
 
   const handleRemoveStory = item => {
-    const newStories = stories.filter(
-      story => item.objectID !== story.objectID
-    );
-    setStories(newStories);
+    dispatchStories({
+      type: 'REMOVE_STORY',
+      payload: item,
+    });
   };
 
   // A) A callback gets introduced
