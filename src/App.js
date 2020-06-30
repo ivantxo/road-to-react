8da464f132e09ad27bd1a5d93a1a5f23f067b8f9
ivantxo <ivan.ramirezf@gmail.com>
2020-07-01 02:12:24 +1000

    Data Fetching 1

--- src/App.js
+++ src/App.js
@@ -1,52 +1,7 @@
 import React from 'react';
 import './App.css';
 
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: 'PHP',
-    url: 'https://www.php.net',
-    author: 'Rasmus Lerdorf',
-    num_comments: 3,
-    points: 4,
-    objectID: 2,
-  },
-  {
-    title: 'git',
-    url: 'https://git-scm.com/',
-    author: 'Linus Torvalds',
-    num_comments: 2,
-    points: 6,
-    objectID: 3,
-  },
-];
-
-const getAsyncStories = () =>
-  new Promise(resolve =>
-    setTimeout(
-      () => resolve({ data: { stories: initialStories } }),
-      2000
-    )
-  );
-
-// Simulate an error
-// const getAsyncStories = () =>
-//   new Promise((resolve, reject) => setTimeout(reject, 2000));
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -108,11 +63,12 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`)
+      .then(response => response.json())
       .then(result => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() =>
@@ -180,36 +136,6 @@ const InputWithLabel = ({ id, value, type = 'text', onInputChange, children }) =
     />
   </>
 );
-
-// const List = ({ list }) =>
-//   list.map(item => 
-//     <Item 
-//       key={ item.objectID }
-//       title={ item.title }
-//       url={ item.url }
-//       author={ item.author }
-//       num_comments={ item.num_comments }
-//       points={ item.points }
-//     />);
-
-// Above can be replaced and simplified with JS spread operator
-// const List = ({ list }) =>
-//   list.map(item => <Item key={ item.objectID } { ...item } />);
-
-// Another level of abstraction: rest operator to destructure the objectID from the rest of the item object
-// const List = ({ list }) =>
-//   list.map(({ objectID, ...item }) => <Item key={objectID} { ...item } />);
-
-// const Item = ({ title, url, author, num_comments, points }) => (
-//   <div>
-//     <span>
-//       <a href={ url }>{ title }</a>
-//     </span>
-//     <span>{ author }</span>
-//     <span>{ num_comments }</span>
-//     <span>{ points }</span>
-//   </div>
-// );
 
 const List = ({ list, onRemoveItem }) =>
   list.map(item => (
