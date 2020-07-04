7cfcdfb318898e7f2d7043a53155b46d018963c6
ivantxo <ivan.ramirezf@gmail.com>
2020-07-05 07:31:47 +1000

    Added async/await

--- src/App.js
+++ src/App.js
@@ -65,19 +65,19 @@ const App = () => {
   );
 
   // A
-  const handleFetchStories = React.useCallback(() => { // B
+  const handleFetchStories = React.useCallback(async() => { // B
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
-    axios
-      .get(url)
-      .then(result => {
-        dispatchStories({
-          type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.hits,
-        });
-      })
-      .catch(() =>
-        dispatchStories({ type: 'STORIES_FECTH_FAILURE' })
-      );
+
+    try {
+      const result = await axios.get(url);
+
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: result.data.hits,
+      });
+    } catch {
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
+    }
   }, [url]);
 
   React.useEffect(() => {
