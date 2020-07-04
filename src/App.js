663996ae7f61fb2a12c661c22fdb629615384a72
ivantxo <ivan.ramirezf@gmail.com>
2020-07-05 07:24:38 +1000

    Adding axios instead of fetch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import axios from 'axios';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -67,17 +67,16 @@ const App = () => {
   // A
   const handleFetchStories = React.useCallback(() => { // B
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
-
-    fetch(url)
-      .then(response => response.json())
+    axios
+      .get(url)
       .then(result => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits,
+          payload: result.data.hits,
         });
       })
       .catch(() =>
-        dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
+        dispatchStories({ type: 'STORIES_FECTH_FAILURE' })
       );
   }, [url]);
 
